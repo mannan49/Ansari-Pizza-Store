a7a6454d5210dde0bb3fb0a0e294407f0f070e9f
mannan49 <mannannasir49@gmail.com>
2023-02-15 07:47:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation";
+import CartPage from "./Components/CartPage";
+import Home from "./Components/Home";
+import SeparateProduct from "./Components/SeparateProduct";
+import ProductsPage from "./Components/ProductsPage";
+import { CartContext } from "./CartContext";
+import { useEffect, useState } from "react";
+function App() {
+  const [cart,setCart] = useState({});
+  useEffect(()=>{
+    const cart = window.localStorage.getItem('cart')
+    setCart(JSON.parse(cart))
+  },[])
+
+  useEffect(()=>{
+    window.localStorage.setItem('cart', JSON.stringify(cart))
+  },[cart])
+
+  return (
+    <Router>
+      <CartContext.Provider value={{ cart, setCart}}>
+        <div className="App">
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/products" element={<ProductsPage />}></Route>
+            <Route path="/products/:_id" element={<SeparateProduct />}></Route>
+            <Route path="/cart" element={<CartPage />}></Route>
+          </Routes>
+        </div>
+      </CartContext.Provider>
+    </Router>
+  );
+}
+
+export default App;
